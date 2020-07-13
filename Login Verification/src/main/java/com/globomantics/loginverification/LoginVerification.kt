88c67aff0e5863cfdcb61f14e74a0a7e34d40a6d
Danko Papucic <dpapucic@hrsinternational.com>
2020-07-13 15:37:26 +0200

    Change method name

--- Login Verification/src/main/java/com/globomantics/loginverification/LoginVerification.kt
+++ Login Verification/src/main/java/com/globomantics/loginverification/LoginVerification.kt
@@ -1,7 +1,7 @@
 package com.globomantics.loginverification
 
 class LoginVerification {
-    fun verify(email: string, password: string): Boolean {
+    fun verifyEmailAndPassword(email: string, password: string): Boolean {
         return (email == "test@globomantics.com" &&
                 password == "test123") ||
                 (email == "admin@globomantics.com" &&
